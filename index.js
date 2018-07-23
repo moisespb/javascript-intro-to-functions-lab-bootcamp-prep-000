1872bc2747b2b2f77d4169646155d9b1232b7080
Moises Perez-Batista <moisesdpbjr@gmail.com>
2018-07-23 20:18:09 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,24 @@
+function shout(string) {
+  return 'hello'.toUpperCase()
+}
+
+function whisper(string) {
+  return 'hello'.toLowerCase()
+}
+
+function logShout(string) {
+  console.log('HELLO')
+  'Hello!'.toUpperCase()
+}
+
+function logWhisper(string) {
+  console.log('hello')
+  'Hello!'.toLowerCase()
+}
+
+function sayHiToGrandma(string){
+var lowercase = string;
+if ('')string.toLowerCase();
+return "I can't hear you!".toLowerCase();
+
+}
